dec0a5ca165853a36eae0e8a8b2439fcb0b62155
Mason.S98 <120329380+reverandMason999@users.noreply.github.com>
2023-05-30 20:42:14 -0400

    first push

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = 5000;
+const hostname = "127.0.0.1";
+
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
