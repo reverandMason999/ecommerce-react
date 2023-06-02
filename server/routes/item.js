d0ef42072f78416e40be7f995e3c09606124d697
Mason.S98 <120329380+reverandMason999@users.noreply.github.com>
2023-06-01 20:49:46 -0400

    created routes and data tables

---
+++ server/routes/item.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const { Item } = require('../models');
+
+router.get('/item', async (req, res) => {
+    const items = await Item.findAll()
+    res.json(items)
+});
+
+router.get('/item/:id', async (req, res) => {
+    const item = await Item.findByPk(req.params.id)
+    res.json(item)
+});
+
+router.post('/item', async (req, res) => {
+    const { name, price, category, img } = req.body;
+    const newItem = await Item.create({
+        name,
+        price,
+        category,
+        img
+    });
+    res.json(newItem)
+});
+
+router.put('/item/:id', async (req, res) => {
+    const { id } = req.params;
+    const updatedItem = await Item.update(req.body, {
+        where: {
+            id,
+        }
+    });
+    res.json(updatedItem);
+});
+
+
+router.delete('/item/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedItem = await Item.destroy({
+        where: {
+            id,
+        }
+    });
+    res.json(deletedItem);
+});
+module.exports = router;
