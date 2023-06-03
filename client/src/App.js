0a012b791d0a98337cbd290fa6fc508e668d7dd9
Addy Severe <adelaidecantave@gmail.com>
2023-06-03 11:53:52 -0400

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
 import TopBar from './components/TopBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Buy from "./pages/Buy";
+import Checkout from "./pages/Checkout";
+import NewItem from "./pages/NewItem";
+import Product from "./pages/Product";
+import ProductSearch from "./pages/ProductSearch";
+import Sell from "./pages/Sell";
+import SignUp from "./pages/SignUp";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <TopBar/>
-      </header>
-    </div>
+    <> 
+    <TopBar/>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/buy" element={<Buy />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/newitem" element={<NewItem />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productsearch" element={<ProductSearch />} />
+        <Route path="/sell" element={<Sell />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,10 +1,12 @@
-import './TopBar.css';
+import * as React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './TopBar.css';
 
 function TopBar() {
   return (
