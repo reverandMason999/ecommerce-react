fa61cc41693d01e4277d0f726a3dbcb32a1902c5
Alexander Forde <alexanderforde12@gmail.com>
2023-06-12 22:23:44 -0400

    trying to debug

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -1,83 +1,86 @@
-import React, {useState} from 'react'
-import { CardElement, useElements, useStripe} from "@stripe/react-stripe-js"
-import axios from "axios"
+import React, { useState } from 'react';
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import axios from "axios";
 
-
-const Card_OPTIONS ={
-    iconStyle: "solid",
-    style: {
-        base: {
-            iconcolor: "#c4f0ff",
-            color: "#fff",
-            fontweight: 500,
-            fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-            fontSize: "16px",
-
-        },
-        invalid: {
-            iconcolor: "#ffc7ee",
-            color: "#ffc7ee"
-        }
-        }
+const Card_OPTIONS = {
+  iconStyle: "solid",
+  style: {
+    base: {
+      iconColor: "#c4f0ff",
+      color: "#fff",
+      fontWeight: 500,
+      fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+      fontSize: "16px",
+    },
+    invalid: {
+      iconColor: "#ffc7ee",
+      color: "#ffc7ee"
     }
+  }
+};
 
 export default function PaymentForm() {
-    const [success, setSucess ] = useState(false)
-    const stripe = useStripe()
-    const elements = useElements()
+  const [success, setSuccess] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
 
-    const [state, setState] = useState({
-        CardNumber: '',
-        ExpDate: '',
-        CVC: ''
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card: elements.getElement(CardElement),
     });
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const {error, paymentMethod} = await stripe.createPaymentMethod({
-            type: "card" , 
-            card: elements.getElement(CardElement)
-        })
-    
+    if (!error) {
+      try {
+        const { id } = paymentMethod;
+        console.log(id);
 
-if (!error) {
-    try { 
-        const {id} = paymentMethod
-        const response = await axios.post("http://localhost:5000/payment", {withCredentials:true}, {
+        const response = await axios.post(
+          "http://localhost:5000/payment",
+          {
             amount: 1000,
             id
-        })
+          },
+          {
+            withCredentials: true,
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
 
-    if(response.data.success) {
-        console.log("Successful payment")
-        setSucess(true)
-    }
+        const responseData = response.data;
+        console.log(responseData);
 
-} catch (error) {
-    console.log("Error", error )
-}
-
-} else {
-    console.log(error.message)
-}
-
-}
-return (
-    <>
-    {!success ?
-    <form onSubmit = {handleSubmit}>
-        <fieldset className="FormGroup">
-            <div className='FormRow'>
-            <CardElement options={Card_OPTIONS}/>
-            </div>
-            </fieldset>
-            <button>Pay</button>
-        </form>
-    :
-        <div>
-            <h2>You just bought an Item!</h2>
-        </div>
+        if (responseData.success) {
+          setSuccess(true);
+        } else {
+          // Handle payment failure
         }
-    </>
-)
+      } catch (error) {
+        console.error(error);
+      }
     }
+  };
+
+  return (
+    <>
+      {!success ? (
+        <form onSubmit={handleSubmit}>
+          <fieldset className="FormGroup">
+            <div className='FormRow'>
+              <CardElement options={Card_OPTIONS} />
+            </div>
+          </fieldset>
+          <button>Pay</button>
+        </form>
+      ) : (
+        <div>
+          <h2>You just bought an Item!</h2>
+        </div>
+      )}
+    </>
+  );
+}
