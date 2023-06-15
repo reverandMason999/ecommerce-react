303a0eb2ffd064b0dcb15fd0096a8ea4d8c8465b
Mason.S98 <120329380+reverandMason999@users.noreply.github.com>
2023-06-15 13:27:06 -0400

    added first bit of input validation

--- server/routes/register.js
+++ server/routes/register.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { User } = require('../models')
 const rateLimit = require('express-rate-limit');
+const { check, validationResult } = require('express-validator');  
 
 const registerLimit = rateLimit({
     windowMs: 25 * 60 * 1000, // 25 minutes
@@ -11,14 +12,32 @@ const registerLimit = rateLimit({
 	legacyHeaders: false, 
 });
 
-router.post('/register', registerLimit, async (req, res) => { 
-    const { username, email, password } = req.body;
-    const newUser = await User.create({
-        username,
-        email,
-        password
-    })
-    res.json(newUser)
-});
+  
+router.post('/register', registerLimit, [
+    check('username', 'Username must be at least 5 characters').not().isEmpty(),
+    check('email', 'Email must be valid').not().isEmpty().isEmail().normalizeEmail(),
+    check('password', 'Password must be at least 5 characters').not().isEmpty().isLength({min: 5}).trim()
+    ],async (req, res) => { 
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.status(422).json({ errors: errors.array() }) 
+            return
+        }
+        try{
+        const { username, email, password } = req.body;
+        const newUser = await User.create({
+            username,
+            email,
+            password
+        })
+        res.json(newUser)
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({ error: "Server error" });
+    }
+        
+    });
+    
+  
 
 module.exports = router;
