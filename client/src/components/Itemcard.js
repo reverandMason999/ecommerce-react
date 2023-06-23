b1bf70cc59d13664ba58b49643d68da9719c9f01
Mason.S98 <120329380+reverandMason999@users.noreply.github.com>
2023-06-23 16:19:24 -0400

    got things set up for the most part, still requires some more work to get the list displaying

--- client/src/components/Itemcard.js
+++ client/src/components/Itemcard.js
@@ -1,27 +1,19 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+
 import './User.css'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-const Itemcard = () => {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        axios.get('/item').then(res => {
-            console.log(res)
-            setData(res.data)})
-        .catch(err => console.log(err))
-    }, [])
-
+const Itemcard = ({ items }) => {
+    
     return( 
       <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>{data.name}</Card.Title>
+        <Card.Title>{items.name}</Card.Title>
         <Card.Text>
-        {data.description}
+        {items.description}
         <Card.Text>
-        ${data.price}
+        ${items.price}
         </Card.Text>
         </Card.Text>
         <Button variant="primary">Buy</Button>
